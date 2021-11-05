32599d159b8a35cde9724355c67e70277205aec9
chingkai5566 <h14577@gmail.com>
2021-11-05 10:11:57 -0400

    Init cf metadata

--- config.py
+++ config.py
@@ -1,12 +1,18 @@
 """
 Global Configuration for Application
 """
+import json
 import os
 
 # Get configuration from environment
 DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgres://postgres:postgres@localhost:5432/postgres"
 )
+
+# override if we are running in Cloud Foundry
+if 'VCAP_SERVICES' in os.environ:
+    vcap = json.loads(os.environ['VCAP_SERVICES'])
+    DATABASE_URI = vcap['user-provided'][0]['credentials']['url']
 
 # Configure SQLAlchemy
 SQLALCHEMY_DATABASE_URI = DATABASE_URI
